99db37b05e31d3f1e87a6de38350e530a0a5ace5
z0ukun <z0ukun@163.com>
2018-11-22 22:13:13 +0800

    H5

---
+++ H5/booktest/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=20)
+    bpub_date = models.DateTimeField(db_column='pub_date')
+    bread = models.IntegerField(default=0)
+    bcommet = models.IntegerField(null=False)
+    is_Delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+
+
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=10)
+    hgender = models.BooleanField(default=True)
+    hcontent = models.CharField(max_length=1000)
+    is_Delete = models.BooleanField(default=False)
+    book = models.ForeignKey(BookInfo)
